559059f5342e361415db5a01cf0c633541384bd5
maxiibrunaa123 <maxiibrunaa123@gmail.com>
2022-10-20 05:03:06 -0300

    New Commit

--- src/com/company/ClaseMain.java
+++ src/com/company/ClaseMain.java
@@ -1,2 +1,31 @@
-package com.company;public class ClaseMain {
+package com.company;
+
+public class ClaseMain {
+    public static void main(String[] args) {
+
+        byte numberbyte = 1;
+        short numbershort = 22222;
+        int numberint = 333333333;
+        long numberlong = 444444444;
+        float numberfloat = 5555555555.0f;
+        double numberdouble = 66666666666666.0d;
+        char numberchar = 'a';
+        boolean numberboolean = true;
+        boolean numberbooleanf = false;
+        String string = "String";
+
+        System.out.println("Esto es un byte: " + numberbyte);
+        System.out.println("Esto es un short: " + numbershort);
+        System.out.println("Esto es un int: " + numberint);
+        System.out.println("Esto es un long: " + numberlong);
+        System.out.println("Esto es un float: " + numberfloat);
+        System.out.println("Esto es un double: " + numberdouble);
+        System.out.println("Esto es un char: " + numberchar);
+        System.out.println("Esto es un boolean: " + numberboolean);
+        System.out.println("Esto es un boolean negativo: " + numberbooleanf);
+        System.out.println("Esto es un string: " + string);
+
+
+    }
+
 }
